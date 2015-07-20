1abf0c27c9613865b92750e81c8b2d1fa3ab669b
Dmitry Shubin <shubin@ksyslabs.org>
2015-07-20 20:51:50 +0300

    org-summary-todo

--- config/org.el
+++ config/org.el
@@ -7,6 +7,12 @@
 			("\\.mm\\'" . default)
 			("\\.x?html?\\'" . (browse-url-firefox file))
 			("\\.pdf\\'" . default)))
+  (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
   (add-hook 'org-mode-hook
 	    (lambda ()
 	      (visual-line-mode t))))
+
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
